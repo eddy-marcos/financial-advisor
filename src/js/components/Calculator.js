d15c5effe87de635b518e2e4e420a73ef11f2974
Eddy Marcos <emarcos@rpa.com>
2021-02-18 14:41:07 -0600

    page no found error bug fix

--- src/js/components/Calculator.js
+++ src/js/components/Calculator.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useSelector } from "react-redux";
 import RiskLevelOverview from './RiskSelector/RiskLevelOverview';
 import RiskCalculator from './RiskCalculator/RiskCalculator';
 
 const Calculator = (props) => {
   const activeRiskLevel = useSelector(state => state.riskLevels.activeRiskLevel);
+  
+  useEffect(() => {
+    window.onbeforeunload = function() { 
+      setTimeout(function () { 
+        window.location = '/';
+    }, 0); 
+
+    // necessary to prevent infinite loop
+    window.onbeforeunload = null; 
+    }
+  }, [])
+
+
   return (
     <div>
       <h2 className="text-center">Personalized Portfolio</h2>
